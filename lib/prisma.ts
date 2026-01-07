498d70913a257924e7512ee9d2289247fc0702d9
mccn5 <kangho.kim@pmgasia.com>
2026-01-07 14:16:56 +0900

    database change to mysql

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
-import { Pool } from "pg"
-import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
 
-const connectionString = `${process.env.DATABASE_URL}`
-
 const prismaClientSingleton = () => {
-    const pool = new Pool({ connectionString })
-    const adapter = new PrismaPg(pool)
-    return new PrismaClient({ adapter })
+    return new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL,
+            },
+        },
+    })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
