233cdf3ae9f404ecc58ac53a33c066fd5ead7f91
mccn5 <kangho.kim@pmgasia.com>
2026-01-07 18:58:39 +0900

    FIX: No 'user_info' cookie found

--- components/auth-guard.tsx
+++ components/auth-guard.tsx
@@ -19,7 +19,7 @@ export function AuthGuard({ children }: { children: React.ReactNode }) {
         const getCookie = (name: string) => {
             const value = `; ${document.cookie}`
             const parts = value.split(`; ${name}=`)
-            if (parts.length === 2) return parts.pop()?.split(";").shift()
+            if (parts.length >= 2) return parts.pop()?.split(";").shift()
             return null
         }
 
