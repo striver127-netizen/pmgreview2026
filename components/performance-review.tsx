75d3d6b0884e2107d064c370e420753081e4fa13
mccn5 <kangho.kim@pmgasia.com>
2026-01-27 15:39:20 +0900

    123123

--- components/performance-review.tsx
+++ components/performance-review.tsx
@@ -27,6 +27,7 @@ export default function PerformanceReview() {
       if (target.target_grant === "leader") return "Type2"
       return "Type1"
     } else {
+      if (target.target_type === "TL") return "Type2"
       //if (target.target_grant === "leader") return "Type2"
       const teamIds = ["4", "42", "43", "44", "38", "35"]
       if (target.target_team_id && teamIds.includes(String(target.target_team_id))) return "Type3"
